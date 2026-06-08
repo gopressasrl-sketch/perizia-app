cbee8478a637abd54a3eb27352614fc5221064df
gopressasrl-sketch <gopressasrl-sketch@users.noreply.github.com>
2026-06-08 15:41:43 +0000

    deploy: b9f5460f33f4932b14f4a7db1096de058bafb5a1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -51,7 +51,7 @@ if (flet.noCdn) {
 _flutter.loader.load({
     config: flutterConfig,
     serviceWorkerSettings: {
-        serviceWorkerVersion: "292671012" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+        serviceWorkerVersion: "2947010324" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
     },
     onEntrypointLoaded: async function (engineInitializer) {
         const engine = await engineInitializer.initializeEngine(flutterConfig);
